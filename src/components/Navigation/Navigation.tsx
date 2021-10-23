d757d2806a487c76f01d1ae28ba184fd53585405
IamDarya <75080122+IamDarya@users.noreply.github.com>
2021-10-23 22:44:40 +0200

    refactor: move comp

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Logo } from './Logo/Logo';
+import { Logo } from '../Logo/Logo';
 
 const Nav = styled.div`
   display: flex;
