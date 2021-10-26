101e379a05ea56dce93a35d8762377b36ac246fc
IamDarya <75080122+IamDarya@users.noreply.github.com>
2021-10-26 23:19:15 +0200

    feat: reconditioned face box styles

--- src/components/FaceRecognition/FaceRecognition.tsx
+++ src/components/FaceRecognition/FaceRecognition.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Wrapper } from '../ImageLinkForm/ImageLinkForm';
+import Box from '../models/Box';
 
 const Wrap = styled(Wrapper)`
   padding: 0;
@@ -9,15 +10,29 @@ const Wrap = styled(Wrapper)`
 const ImageView = styled.img`
   height: 55vh;
 `;
+export const ReconditionedFaceBox = styled.div<{ box: Box }>`
+  position: absolute;
+  box-shadow: 0 0 0 3px #ffd849 inset;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  cursor: pointer;
+  top: ${({ box }) => box.topRow + 'px'};
+  right: ${({ box }) => box.rightCol + 'px'};
+  bottom: ${({ box }) => box.bottomRow + 'px'};
+  left: ${({ box }) => box.leftCol + 'px'};
+`;
 
 type Props = {
   imageURL: string;
+  box: Box;
 };
 
-export const FaceRecognition = ({ imageURL }: Props): JSX.Element => {
+export const FaceRecognition = ({ imageURL, box }: Props): JSX.Element => {
   return (
     <Wrap>
       <ImageView id="image" src={imageURL} alt="" />
+      <ReconditionedFaceBox box={box}></ReconditionedFaceBox>
     </Wrap>
   );
 };
